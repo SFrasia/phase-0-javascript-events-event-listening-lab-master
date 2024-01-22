6057a3fd236dd3627546a02ee1752a115b5ebeca
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-22 14:10:47 +0300

    changes

---
+++ index.js
@@ -0,0 +1,12 @@
+// const input = document.getElementById("input");
+// input.addEventListener("click", function () {
+//   alert("I was clicked!");
+// });
+
+const input = document.getElementById("input");
+
+function clickAlert() {
+  alert("I was clicked!");
+}
+
+input.addEventListener("click", clickAlert);
